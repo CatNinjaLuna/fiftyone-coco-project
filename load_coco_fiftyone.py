0194d338c5b965bffc9db5c732c07f93c1ad1f7f
“CarolinaLiPage” <caro1650ds@gmail.com>
2026-01-27 19:49:53 -0800

    Fix: Update dataset path and type to FiftyOneDataset format

--- load_coco_fiftyone.py
+++ load_coco_fiftyone.py
@@ -1,14 +1,14 @@
 import fiftyone as fo
 
 name = "coco-local"
-dataset_dir = "/absolute/path/to/coco"
+dataset_dir = "/Users/carolina1650/fiftyone-coco-project/quickstart"
 
 if name in fo.list_datasets():
     fo.delete_dataset(name)
 
 dataset = fo.Dataset.from_dir(
     dataset_dir=dataset_dir,
-    dataset_type=fo.types.COCODetectionDataset,
+    dataset_type=fo.types.FiftyOneDataset,
     name=name,
 )
 
